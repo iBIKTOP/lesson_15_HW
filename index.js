7b94924d694db580f6be5bdf37bf8f5eb23c5f65
BIKTOPBB <BIKTOPBB@gmail.com>
2018-07-15 15:11:47 +0300

    первое задание

--- index.js
+++ index.js
@@ -2,24 +2,36 @@ var Event = require("events").EventEmitter;
 var emt = new Event();
 var http = require('http');
 var fs = require('fs');
+function getDate() {
+	var d = new Date();
+    var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    return d.toLocaleString("ru", options);
+}
+
+emt.on('login', function() {
+    fs.appendFile('logger.txt', getDate() + ' Пользователь залогинился!\r\n', function () {
+        console.log(getDate() + ' Пользователь залогинился! Данные в logger.txt обновлены!');
+    });
+});
+emt.on('active', function() {
+    fs.appendFile('logger.txt', getDate() + ' Пользователь находится на странице!\r\n', function () {
+        console.log(getDate() + ' Пользователь находится на странице! Данные в logger.txt обновлены!');
+    });
+});
+emt.on('about', function() {
+    fs.appendFile('logger.txt', getDate() + ' Пользователь запросил данные о себе!\r\n', function () {
+        console.log(getDate() + ' Пользователь запросил данные о себе! Данные в logger.txt обновлены!');
+    });
+});
 
 http.createServer(function (req,res) {
-	emt.once('login', function() {
-		fs.appendFile('logger.txt', Date() + ' Пользователь залогигился!\r\n', function () {
-			console.log(Date() + 'Пользователь залогигился! Данные в logger.txt обновлены!');
-		}); 	
-	});
-	emt.once('active', function() {
-		fs.appendFile('logger.txt', Date() + ' Пользователь находится на странице!\r\n', function () {
-			console.log(Date() + 'Пользователь находится на странице! Данные в logger.txt обновлены!');
-		}); 	
-	});
-	emt.once('about', function() {
-		fs.appendFile('logger.txt', Date() + ' Пользователь запросил данные о себе!\r\n', function () {
-			console.log(Date() + 'Пользователь запросил данные о себе! Данные в logger.txt обновлены!');
-		}); 	
-	});
-
 	res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
 		if (req.url==='/'){
 			res.write("<a href='/login'>Вход</a><br>");
@@ -34,9 +46,7 @@ http.createServer(function (req,res) {
 			res.write("<a href='/'>Выход</a>");
 			emt.emit('about');
 		}
-
 	res.end();
-	// if (req.url==='/login') emt.emit('login');
 }).listen(3000, function () {
 	console.log('Серевер работает на localhost:3000');
 });
